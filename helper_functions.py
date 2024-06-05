fa448834561f955a3dac86cd38b771565b2c8166
glebr <glebrostanin@gmail.com>
2024-06-05 04:35:03 +0200

    TODO: RUN ALL TRAINING WITH MORE EPOCHS; Unified Attack training functions;

--- helper_functions.py
+++ helper_functions.py
@@ -12,6 +12,7 @@ class DatasetClassN (Enum):
     Cifar = 10
     Tinyimage = 200
 
+# Good Old Training function
 def train_loop (num_epochs:int, model:nn.Module, dataloader:DataLoader, optimizer:optim, criterion:nn, device:d) -> nn.Module:
     model.train()
     print ("Starting Training!")
@@ -39,45 +40,69 @@ def train_loop (num_epochs:int, model:nn.Module, dataloader:DataLoader, optimize
         average_loss = running_loss / total_batches
         print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {average_loss:.4f}')
 
+def print_first_sample (sample_number, inputs, labels, outputs, input_squeeze=None, loss=None, top_sigmoids=None):
+    print ("------SAMPLE WINDOW---------------------------------------------------------")
+    print (f"Number Samples: {sample_number}")
+    print (f"Batchsize: {inputs.size(0)}")
+    inp_str = f"{inputs}"
+    print ("Inputs:" + inp_str.replace('\n',"").replace("   ",""))
+    if input_squeeze is not None:
+        print (f"Squeezed Input: {input_squeeze}")
+    labels_str = f"{labels}"
+    print ("Labels:" + labels_str.replace('\n',"").replace("    ",""))
+    outputs_str = f"{outputs}"
+    print ("Outputs:" + outputs_str.replace('\n',"").replace("  ",""))
+    if loss is not None:
+        print (f"Loss:{loss}")
+    if top_sigmoids is not None:
+        print (f"Top Sigmoids: {top_sigmoids}")
+    print ("---------------------------------------------------------------------------")
 
-
-def train_or_load(model:nn.Module, train_loader:DataLoader, eval_loader:DataLoader, optimizer:optim, criterion:nn, epochs:int, save_path:str=None, device:str="cpu"):
+def train_or_load_and_eval_atck_model(model:nn.Module, train_loader:DataLoader, eval_loader:DataLoader, optimizer:optim, criterion:nn, epochs:int, save_path:str=None, device:str="cpu"):
+    # Check if attack model file already available
     if save_path is not None and isfile (save_path):
         print (f'Attack model state dictionary already available, loading into input model from {save_path}!')
         model.load_state_dict(torch.load(save_path))
+    # Train + test on eval and Save attack model
     else:
-        model.train()
+        first_sample = True
         for epoch in range(epochs):
             print (f'Epoch: {epoch+1}')
-            for idx, (inputs, labels) in enumerate(train_loader, 0):
+            model.train()
+            train_loss = 0.0
+            sample_n = len(train_loader.sampler)
+            for inputs, labels in train_loader:
                 inputs = inputs.float().to(device)  # Ensures input tensors are floats
                 labels = labels.float().to(device).view(-1, 1)  # Ensures labels are floats and reshaped correctly
-
+                
                 optimizer.zero_grad()
                 outputs = model(inputs.squeeze(dim=1))
                 loss = criterion(outputs, labels)
+                if first_sample:
+                    print_first_sample (sample_n, inputs, labels, outputs, inputs.squeeze(dim=1), loss)
+                    first_sample = False
                 loss.backward()
                 optimizer.step()
-                if idx % (50) == 0:
-                    print (inputs[0])
-                    print (labels[0])
-            print(f"Loss: {loss.item()}")
-
-            evaluate_attack_model(model,eval_loader,1,"cpu")
+                # update running training loss
+                train_loss += loss.item()*inputs.size(0)
+            print(f"Epoch Loss: {train_loss/sample_n}")
+            evaluate_attack_model(model,eval_loader,device)
+        print ("----------^^^^\nFinished Training!")
         # Save trained model if savepath not None
         if save_path is not None:
-            print (f'Finished Training\n Saving model state dictionary to path {save_path}!')
+            print (f'Saving model state dictionary to path {save_path}!')
             try: 
                 torch.save(model.state_dict(), save_path)
             except:
                 print ("Saving failed due to exception!")
+        else:
+            print ('Not saving model as save_path is None!')
                 
                 
-############################################ FOR JUPYTER NOTEBOOOK
 # Use this function to create a training dataset for the attack model, based on shadow training/testing datasets
-def create_post_train_loader (non_memb_loader:DataLoader, memb_loader:DataLoader, shadow_model:nn.Module, batch_size:int, multi_n:int, device, save_path:str=None)->DataLoader:
+def create_shadow_post_train_loader (non_memb_loader:DataLoader, memb_loader:DataLoader, shadow_model:nn.Module, batch_size:int, multi_n:int, device, save_path:str=None)->DataLoader:
     if save_path is not None and isfile(save_path):
-        print (f"Attack dataset was already established previosly, loading dataset from {save_path}.")
+        print (f"Attack dataset was already established previosly, loading dataset from \"{save_path}\".")
         # Load already established dset
         with open(save_path, "rb") as att_dset_f:
             dataset_attack = pickle.load(att_dset_f)
@@ -86,6 +111,8 @@ def create_post_train_loader (non_memb_loader:DataLoader, memb_loader:DataLoader
         dataset_attack = []
         # NON Members
         with torch.no_grad():
+            first_sample = True
+            non_memb_num_samples = len(non_memb_loader.sampler)
             for images, labels in non_memb_loader: #need only one
                     # Move images and labels to the appropriate device
                 images, labels = images.to(device), labels.to(device)
@@ -93,10 +120,17 @@ def create_post_train_loader (non_memb_loader:DataLoader, memb_loader:DataLoader
                 logits = shadow_model(images)
                 #take the 3 biggest logist
                 top_values = torch.topk(logits, k=3).values
-                top_values, indices = torch.sort(top_values, dim=1, descending=True)
-                dataset_attack.append([top_values,0])
+                sorted_sigmoids, _ = torch.sort(top_values.to("cpu"), dim=1, descending=True)
+                dataset_attack.append([sorted_sigmoids,0])
+                # Logging
+                if first_sample:
+                    print ("NON Members")
+                    print_first_sample(non_memb_num_samples, images, labels, logits, input_squeeze=None, loss=None, top_sigmoids=sorted_sigmoids)
+                    first_sample = False
         # MEMBERS
         with torch.no_grad():
+            first_sample = True
+            memb_num_samples = len(non_memb_loader.sampler)
             for images, labels in memb_loader: #need only one
                     # Move images and labels to the appropriate device
                 images, labels = images.to(device), labels.to(device)
@@ -104,12 +138,17 @@ def create_post_train_loader (non_memb_loader:DataLoader, memb_loader:DataLoader
                 logits = shadow_model(images)
                 #take the 3 biggest logist
                 top_values = torch.topk(logits, k=3).values
-                top_values, _ = torch.sort(top_values, dim=1, descending=True)
-                dataset_attack.append([top_values,1])
+                sorted_sigmoids, _ = torch.sort(top_values.to("cpu"), dim=1, descending=True)
+                dataset_attack.append([sorted_sigmoids,1])
+                # Logging
+                if first_sample:
+                    print ("Members")
+                    print_first_sample(memb_num_samples, images, labels, logits, input_squeeze=None, loss=None, top_sigmoids=sorted_sigmoids)
+                    first_sample = False
         if save_path is not None:
             # Save dset
             with open (save_path, "wb") as att_dset_f:
-                print (f"Saving attack model training dataset at {save_path}!")
+                print (f"Saving attack model training dataset at \"{save_path}\"!")
                 try: 
                     pickle.dump(dataset_attack, att_dset_f)
                 except:
@@ -119,9 +158,12 @@ def create_post_train_loader (non_memb_loader:DataLoader, memb_loader:DataLoader
     return attack_dtloader
 
 # Use this function to create a dataset of the target model posteriors
-def create_eval_post_loader (target_model:nn.Module, eval_dataloader:DataLoader, multi_n:int=1, device="cpu")->DataLoader:
+def create_eval_post_loader (target_model:nn.Module, eval_dataset, workers_n, device="cpu")->DataLoader:
     target_dataset_eval = []
+    eval_dataloader = DataLoader(eval_dataset, batch_size=1 , shuffle=False, num_workers=1)
     with torch.no_grad():
+        first_sample = True
+        num_samples = len(eval_dataloader.sampler)
         for images,_, member in eval_dataloader: #need only one
             # Move images and labels to the appropriate device
             images = images.to(device)
@@ -129,26 +171,29 @@ def create_eval_post_loader (target_model:nn.Module, eval_dataloader:DataLoader,
             logits = target_model(images)
             #take the 3 biggest logist
             top_values = torch.topk(logits, k=3).values #order poseri
-            sorted_tensor, _ = torch.sort(top_values, dim=1,descending=True)
+            sorted_tensor, _ = torch.sort(top_values.to("cpu"), dim=1,descending=True)
+            if first_sample:
+                    print_first_sample(num_samples, images, member, logits, input_squeeze=None, loss=None, top_sigmoids=sorted_tensor)
+                    first_sample = False
             target_dataset_eval.append([sorted_tensor, member.item()])
-        target_eval_dl = torch.utils.data.DataLoader(target_dataset_eval, batch_size=1 , shuffle=False, num_workers=multi_n)
+    target_eval_dl = torch.utils.data.DataLoader(target_dataset_eval, batch_size=64 , shuffle=False, num_workers=workers_n)
     return target_eval_dl
 
-def evaluate_attack_model(model:nn.Module, post_memb_loader:DataLoader):
+def evaluate_attack_model(model:nn.Module, post_memb_loader:DataLoader, device):
     model.eval()  # Set the model to evaluation mode
     correct = 0
     total = 0
 
     with torch.no_grad():  # Disable gradient computation
         for sorted_logits, members in post_memb_loader:
-            sorted_logits = sorted_logits.float()
+            sorted_logits = sorted_logits.to(device).float()
 
             outputs = model(sorted_logits.squeeze(dim=1))
             predicted = torch.round(outputs)  # Round the outputs to 0 or 1
             total += members.size(0)  # Increment the total count by batch size
-            correct += (predicted == members).sum().item()  # Count correct predictions
+            correct += (predicted == members.to(device)).sum().item()  # Count correct predictions
 
     accuracy = correct / total
     print(f'Accuracy: {accuracy:.2f}')
-    return accuracy
+    # return accuracy
         
